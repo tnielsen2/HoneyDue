c48c3b0f9157bc0cfe6e06ea8fd2d15141c72b7f
tnielsen2 <trentnielsen84@gmail.com>
2024-07-12 07:39:50 -0400

    Update layout, add and run prettier

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ A project management tool for families. Made ideally for me.
 
 This is a repo created for me to create a practical project, I can self-host on CloudFlare ZTNA and use it to manage
 my honey-do list with a digital presence.
-
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,8 +3,8 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+} from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -20,7 +20,7 @@ export default async function CardWrapper() {
 
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
@@ -37,7 +37,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
